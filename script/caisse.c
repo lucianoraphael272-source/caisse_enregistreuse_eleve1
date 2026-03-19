94da6072cbc7d20a40ec7df835758ed40a5cd2d1
lucianoraphael272@gmail.com <lucianoraphael272@gmail.com>
2026-03-19 12:16:41 +0400

    ajout affichage billet

--- script/caisse.c
+++ script/caisse.c
@@ -6,12 +6,30 @@ double calcule_monnaie(double x,double y)
 	return monnaie;
 }
 
+double afficher_monnaie(double rendu) {
+    int billets[] = {2000, 500, 200, 100, 50, 20, 10};
+    int pieces[] = {5, 2, 1};
 
+    int montant = (int)rendu;
 
+    printf("Détail de la monnaie:\n");
 
+    for (int i = 0; i < 7; i++) {
+        if (montant >= billets[i]) {
+            int nb = montant / billets[i];
+            montant %= billets[i];
+            printf("Billet Rs%d x %d\n", billets[i], nb);
+        }
+    }
 
-
-
+    for (int i = 0; i < 3; i++) {
+        if (montant >= pieces[i]) {
+            int nb = montant / pieces[i];
+            montant %= pieces[i];
+            printf("Pièce Rs%d x %d\n", pieces[i], nb);
+        }
+    }
+}
 
 
 
@@ -38,5 +56,6 @@ int main() {
 
 double rendu = calcule_monnaie(montant , prix);
 printf("Total a rendre est %.2f\n", rendu);
-    return 0;
+afficher_monnaie(rendu);   
+ return 0;
 }
